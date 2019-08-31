98584fe9a23984f91d3c2140d5838fd36f849422
Jacques <jacques.vanvuren@tutanota.com>
2019-08-31 23:56:20 +1000

    Added and setup Player Controller
    Can walk, jump and run. No animations tied to movement yet.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 20f;
+
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Check if Run button is held
+        if (Input.GetButtonDown("Run"))
+        {
+            runSpeed *= 2f;
+        }
+        else if (Input.GetButtonUp("Run"))
+        {
+            runSpeed = 20f;
+        }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move Ery
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+}
