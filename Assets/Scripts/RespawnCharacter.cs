2a23f1ed6f90c2df8154a774e67e45227801d4c3
Jacques <jacques.vanvuren@tutanota.com>
2019-09-02 15:58:50 +1000

    Fixed camera, fixed tilemap collider & created character respawn

---
+++ Assets/Scripts/RespawnCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnCharacter : MonoBehaviour
+{
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Transform character;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        character.transform.position = respawnPoint.transform.position;
+    }
+}
