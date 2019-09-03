6db2cc86cc377ee5593590b0d05ceccfce18bb37
Jacques <jacques.vanvuren@tutanota.com>
2019-09-03 20:45:43 +1000

    Fixed Enemy Baccy patrol for wall and ledge.
    Also fixed the bug where Ery still runs on the spot when not moving.

---
+++ Assets/Scripts/BaccyPlatPatrol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaccyPlatPatrol : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    public int xMoveDirection;
+    public Transform EdgeCheck;
+
+
+    private void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * speed;
+
+        RaycastHit2D hit = Physics2D.Raycast(EdgeCheck.position, Vector2.down, distance);
+        
+        if (!hit)
+        {
+            FlipX();
+        }
+    }
+
+    void FlipX()
+    {
+        if (xMoveDirection > 0)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            xMoveDirection = -1;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            xMoveDirection = 1;
+        }
+    }
+}
---
+++ Assets/Scripts/BaccyWallPatrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Source
+// https://www.youtube.com/watch?v=aRxuKoJH9Y0
+
+// https://www.youtube.com/watch?v=-mrGHaAdX8M
+
+public class BaccyWallPatrol : MonoBehaviour
+{
+    public float speed;
+    public int xMoveDirection;
+
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * speed;
+
+        if (hit.distance < 0.7f)
+        {
+            FlipX();
+        }
+    }
+
+    void FlipX()
+    {
+        if (xMoveDirection > 0)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            xMoveDirection = -1;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            xMoveDirection = 1;
+        }
+    }
+}
