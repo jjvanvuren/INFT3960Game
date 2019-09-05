f9296a94b1cb1441da4f9439d9fcc16922598de2
Francois Janse van Vuren <c3185723@uon.edu.au>
2019-09-05 18:37:34 +1000

    Added end of level screen
    Reaching the door transfers to this screen

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public int LevelToLoad;
+
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Application.LoadLevel(LevelToLoad);
+        }
+    }
+
+}
