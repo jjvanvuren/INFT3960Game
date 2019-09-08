67909a0a14f14e3cde5e137d622d8ff24ba8ed98
Francois Janse van Vuren <c3185723@uon.edu.au>
2019-09-08 17:43:55 +1000

    Added GameOver screen

--- Assets/Scripts/EryDeath.cs
+++ Assets/Scripts/EryDeath.cs
@@ -11,10 +11,12 @@ public class EryDeath : MonoBehaviour
     public GameObject Ery;
 
     public Animator anim;
+    //private GameOverMenuScript GameOver;
 
     private void Update()
     {
         var player = GameObject.Find("Ery").GetComponent<CharacterController2D>();
+        
         int iLives = player.CharacterLives;
         if (iLives < 1)
         {
@@ -22,14 +24,17 @@ public class EryDeath : MonoBehaviour
             player.CharacterLives = 4;
             GameObject.Find("Ery").GetComponent<PlayerMovement>().enabled = false;
             anim.SetTrigger("Dead");
-            Invoke("Die", 2f);
+            Invoke("GameOver", 1.5f);
+
             
         }
     }
 
-    void Die()
+    void GameOver()
     {
-        character.transform.position = respawnPoint.transform.position;
-        GameObject.Find("Ery").GetComponent<PlayerMovement>().enabled = true;
+        // Show game over menu
+
+        var gameOver = GameObject.Find("Main Camera").GetComponent<GameOverMenuScript>();
+        gameOver.isGameOver = true;
     }
 }
---
+++ Assets/Scripts/GameOverMenuScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOverMenuScript : MonoBehaviour
+{
+    public GameObject GameOverUI;
+    public bool isGameOver = false;
+
+    private void Start()
+    {
+
+        GameOverUI.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        if (isGameOver)
+        {
+
+            GameOverUI.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+
+        if (!isGameOver)
+        {
+
+            GameOverUI.SetActive(false);
+            Time.timeScale = 1;
+
+        }
+    }
+
+    public void Restart()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void TitleScreen()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
