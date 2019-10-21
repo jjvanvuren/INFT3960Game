bfa27dc1acdf20d2112bd35b25abd60255d0fb1c
Francois Janse van Vuren <c3185723@uon.edu.au>
2019-10-21 16:18:54 +1100

    Completed Level 2
    Added moving platforms and new story

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    //Source for player collision: https://www.youtube.com/watch?v=O6wlIqe2lTA
+
+    public Vector3 velocity;
+    public int minPositionY;
+    public int maxPositionY;
+    public bool changeDirection;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move the platform up or down
+        if (transform.position.y <= maxPositionY && !changeDirection)
+        {
+
+            transform.position += velocity * Time.deltaTime;
+        }
+
+        else if (transform.position.y >= minPositionY && changeDirection)
+        {
+
+            transform.position -= velocity * Time.deltaTime;
+        }
+
+
+        // Change direction when reaching max or min position
+        if (transform.position.y > maxPositionY)
+        {
+            changeDirection = true;
+        }
+
+        else if (transform.position.y < minPositionY)
+        {
+            changeDirection = false;
+        }
+
+
+    }
+}
